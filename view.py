19a3d1ce7134d8342428b6aaaf8446ff064e37b9
Valenbu <valentina123acosta@gmail.com>
2025-09-21 23:49:56 -0500

    Arreglos

--- view.py
+++ view.py
@@ -3,12 +3,12 @@ class CalculadoraView:
         a = float(input("Número 1: "))
         b = float(input("Número 2: "))
         return a, b
-    
+
     def pedir_operacion(self):
         return input("Operacion (+, -, *, /): ")
-    
-    def mostrar_resultados(self, resultado):
-        print (f"Resultado:  {resultado}")
+
+    def mostrar_resultado(self, resultado):
+        print(f"Resultado: {resultado}")
 
     def mostrar_error(self, mensaje):
-        print(f"Error {mensaje}")
+        print(f"❌ Error: {mensaje}")
